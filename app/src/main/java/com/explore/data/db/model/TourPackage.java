df24c26e599dd56d7aa9a2c6a3e11186fcbb602c
GBardis <crisolivasavatage@hotmail.com>
2018-10-18 21:26:50 +0300

    Create TourPackage model

--- app/src/main/java/com/explore/data/db/model/TourPackage.java
+++ app/src/main/java/com/explore/data/db/model/TourPackage.java
@@ -1,4 +1,58 @@
 package com.explore.data.db.model;
 
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
 public class TourPackage {
+    @Getter
+    @PrimaryKey
+    public final int id;
+
+    public enum Category {
+        CRETE("Crete"),
+        PELOPONNESE("Peloponnese"),
+        MACEDONIA("Mecodonia"),
+        THESSALY("Thessaly"),
+        THRACE("Threace"),
+        AEAGEAN("Aegean"),
+        IOANIAN("Ioannian"),
+        STEREA_HELLAS("Sterea_hellas");
+
+
+        private final String text;
+
+        Category(final String text) {
+            this.text = text;
+        }
+
+        @Override
+        public String toString() {
+            return text;
+        }
+    }
+
+    @Getter
+    @Setter
+    private String name;
+    @Getter
+    @Setter
+    private Double rating;
+    @Getter
+    @Setter
+    private String ratingColor;
+    @Getter
+    @Setter
+    private String regionColor;
+
+    public TourPackage(int id, String name, Double rating, String ratingColor, String regionColor) {
+        this.id = id;
+        this.setName(name);
+        this.setRating(rating);
+        this.setRatingColor(ratingColor);
+        this.setRegionColor(regionColor);
+    }
 }
--- app/src/main/java/com/explore/data/db/model/User.java
+++ app/src/main/java/com/explore/data/db/model/User.java
@@ -1,4 +1,52 @@
 package com.explore.data.db.model;
 
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
 public class User {
+
+    @PrimaryKey
+    @Getter
+    public final int id;
+    @Getter
+    @Setter
+    private String userName;
+    @Getter
+    @Setter
+    private String password;
+    @Getter
+    @Setter
+    private String firstName;
+    @Getter
+    @Setter
+    private String lastName;
+    @Getter
+    @Setter
+    private String email;
+    @Getter
+    @Setter
+    private String address;
+    @Getter
+    @Setter
+    private Integer age;
+    @Getter
+    @Setter
+    private String teamColor;
+
+    public User(int id, String userName, String password, String firstName, String lastName,
+                String email, String address, Integer age, String teamColor) {
+        this.id = id;
+        this.setUserName(userName);
+        this.setPassword(password);
+        this.setFirstName(firstName);
+        this.setLastName(lastName);
+        this.setEmail(email);
+        this.setAddress(address);
+        this.setAge(age);
+        this.setTeamColor(teamColor);
+    }
 }
