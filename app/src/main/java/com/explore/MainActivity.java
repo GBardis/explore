d630d06c7992ef6781578ca21575285362e2c814
Gbardis <crisolivasavatage@hotmail.com>
2018-10-22 21:21:11 +0300

    Clean up views from unused elements, delete commented code unused from tourPackageFragment

--- app/src/main/java/com/explore/MainActivity.java
+++ app/src/main/java/com/explore/MainActivity.java
@@ -11,9 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-
         getSupportFragmentManager()
                 .beginTransaction()
                 .add(R.id.main_activity_root, new TourPackageFragment())
