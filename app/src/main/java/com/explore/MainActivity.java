a60c77763f4074e25bb42f86939a73a8854f4555
Gbardis <crisolivasavatage@hotmail.com>
2018-10-25 12:29:08 +0300

    Implement navDrawer

--- app/src/main/java/com/explore/MainActivity.java
+++ app/src/main/java/com/explore/MainActivity.java
@@ -1,9 +1,16 @@
 package com.explore;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.explore.features.tour.presentation.TourFragment;
 import com.explore.features.tourpackage.presentation.TourPackageFragment;
@@ -13,10 +20,15 @@ import java.util.Objects;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity implements TourFragment.TourFragmentListener ,
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, TourFragment.TourFragmentListener,
         TourPackageFragment.TourPackageListener {
+
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawer;
+    @BindView(R.id.nav_view)
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +37,24 @@ public class MainActivity extends AppCompatActivity implements TourFragment.Tour
 
         ButterKnife.bind(this);
         // Setup SupportActionBar
-        this.setSupportActionBar(toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_close, R.string.navigation_drawer_open);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        // add image to nav drawer header
+//        View header = navigationView.getHeaderView(0);
+//        ImageView imageView = header.findViewById(R.id.nav_drawer_image);
+//        Picasso.get().load("https://cache-graphicslib.viator.com/graphicslib/thumbs360x240/2916/SITours/city-sightseeing-barcelona-hop-on-hop-off-tour-in-barcelona-534067.jpg")
+//                .resize(100, 100)
+//                .centerCrop()
+//                .into(imageView);
+//
 
         getSupportFragmentManager()
                 .beginTransaction()
@@ -55,5 +84,56 @@ public class MainActivity extends AppCompatActivity implements TourFragment.Tour
                 .addToBackStack(null)
                 .replace(R.id.main_activity_root, new TourPackageFragment())
                 .commit();
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.nav_profile:
+                Toast.makeText(this, "Hi profile", Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.nav_allusers:
+                Toast.makeText(this, "Hi users", Toast.LENGTH_LONG).show();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
