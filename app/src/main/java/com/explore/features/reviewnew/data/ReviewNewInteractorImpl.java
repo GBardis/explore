a506e8e1b38d4983160e67de5e3df3e1c010d6c2
Gbardis <crisolivasavatage@hotmail.com>
2018-10-25 16:49:27 +0300

    Add mapper in review new  interactor

--- app/src/main/java/com/explore/features/reviewnew/data/ReviewNewInteractorImpl.java
+++ app/src/main/java/com/explore/features/reviewnew/data/ReviewNewInteractorImpl.java
@@ -1,5 +1,6 @@
 package com.explore.features.reviewnew.data;
 
+import com.explore.features.reviewnew.domain.ReviewNewDomain;
 import com.explore.features.reviewnew.domain.ReviewNewInteractor;
 import com.explore.features.reviewnew.presentation.ReviewNewUI;
 
@@ -7,6 +8,11 @@ public class ReviewNewInteractorImpl implements ReviewNewInteractor {
 
     @Override
     public void setReviewNew(OnReviewNewSubmitListener onReviewNewSubmitListener, ReviewNewUI reviewNewUI) {
+        ReviewNewDomain reviewNewDomain = new ReviewNewDomain(reviewNewUI.getTitle(),
+                reviewNewUI.getReviewMessage(), reviewNewUI.getRating());
+
+        //Database Call
+
         onReviewNewSubmitListener.onSuccess();
     }
 }
