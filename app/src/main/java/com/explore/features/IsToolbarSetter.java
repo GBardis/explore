3af296df619e189cb0adcdfc7348ad8a48668e6e
Gbardis <crisolivasavatage@hotmail.com>
2018-11-07 18:28:42 +0200

    Optimaze importsd

--- app/src/main/java/com/explore/features/IsToolbarSetter.java
+++ app/src/main/java/com/explore/features/IsToolbarSetter.java
@@ -1,9 +1,6 @@
 package com.explore.features;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
-
-import com.explore.MainActivity;
 
 public interface IsToolbarSetter {
     void setToolbarTitle(Activity activity, String title);
--- app/src/main/java/com/explore/features/tour/domain/TourInteractor.java
+++ app/src/main/java/com/explore/features/tour/domain/TourInteractor.java
@@ -1,15 +1,13 @@
 package com.explore.features.tour.domain;
 
-import com.explore.data.db.model.Tour;
-import com.explore.data.db.model.TourPackage;
 
 import java.util.ArrayList;
 
 public interface TourInteractor {
 
-    void getTourList(OnTourListFinishListener tourPackageFinishListener,String tourPackageId);
+    void getTourList(OnTourListFinishListener tourPackageFinishListener, String tourPackageId);
 
-    interface OnTourListFinishListener{
+    interface OnTourListFinishListener {
         void onTourListSuccess(ArrayList<TourDomain> tourDomainArrayList);
 
         void onError();
